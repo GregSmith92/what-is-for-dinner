9170b3898bd1d608564c7667a7f90b158d94a4e0
Greg Smith <greg_smith1992@hotmail.com>
2024-12-10 15:18:46 +0000

    Add to comment on ingredient parser class

--- app/services/ingredient_parser.rb
+++ app/services/ingredient_parser.rb
@@ -2,9 +2,8 @@ class IngredientParser
   CONFIG = YAML.load_file(Rails.root.join('config', 'units.yml'))
   KNOWN_UNITS = CONFIG['units'].map(&:downcase).uniq
 
-  # This range includes:
-  # U+00BC (¼), U+00BD (½), U+00BE (¾)
-  # and U+2150 through U+2189 cover a variety of fraction forms (⅐, ⅑, ⅒, ⅓, ⅔, etc.)
+  # The numeric regex will capture fractions in multiple formats (e.g. ¼, 0.4, .4)
+  # as well as whole numbers
   NUMERIC_PART = '(?:\d+(?:\.\d+)?|(?:\.\d+)|\d+\s*\d/\d|[\u00BC-\u00BE\u2150-\u2189])'
   QUANTITY_REGEX = /^((?:#{NUMERIC_PART}(?:\s+#{NUMERIC_PART})*))/
 
